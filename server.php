4696c96652b5f7616084e905b264c757e50c644c
Kasper Hartwich <kha@concepy.com>
2013-02-11 17:24:27 +0100

    Can turn off light.

--- server.php
+++ server.php
@@ -1,10 +1,10 @@
 <?php
-$set = isset($_GET['set']) ? (integer)$_GET['set'] : null;
+$set = isset($_GET['set']) ? (integer)$_GET['set'] : false;
 $get = isset($_GET['get']) ? $_GET['get'] : false;
 $id = isset($_GET['id']) ? $_GET['id'] : 'default';
 $allowed = array(0,1,2,3,4,5,6,7);
 
-if ($set!=null && in_array($set, $allowed)) {
+if ($set!==false && in_array($set, $allowed)) {
   echo file_put_contents('./lights/' . $id, $set);
 }
 
