964525b3ac8931c82409101753da710a636b5e50
Kasper Hartwich <kha@concepy.com>
2013-02-11 17:26:05 +0100

    OS supported.

--- handler.php
+++ handler.php
@@ -5,6 +5,7 @@ include dirname(__FILE__) . '/usblight.php';
 if (!file_exists(dirname(__FILE__) . '/usblight.ini')) {die('usblight.ini missing');}
 $ini = parse_ini_file(dirname(__FILE__) . '/usblight.ini');
 
+$os_supported = array('osx');
 $usblight = new usblight($ini['server_url']);
 $current_color = 0;
 
@@ -12,7 +13,9 @@ while (true) {
   $color = $usblight->getColor();
   if ($current_color != $color) {
     $current_color = $color;
-    exec(dirname(__FILE__) . '/light-' . $ini['os'] . ' ' . $color);
+    if (in_array($ini['os'], $os_supported)) {
+      exec(dirname(__FILE__) . '/light-' . $ini['os'] . ' ' . $color);
+    }
     echo 'Changed color: ' . $color . "\n";
   }
   sleep(5);
