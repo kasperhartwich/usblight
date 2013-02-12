569c24f94d2d35812302c7793203499fe7f0f670
Kasper Hartwich <kha@concepy.com>
2013-02-12 13:42:51 +0100

    Multithreading sleep.

--- client.php
+++ client.php
@@ -6,5 +6,17 @@ $ini = parse_ini_file(dirname(__FILE__) . '/usblight.ini');
 
 $color = isset($argv[1]) ? $argv[1] : 0;
 
+$seconds = isset($argv[2]) ? $argv[2] : false;
+
+$sleep = isset($argv[3]) ? true : false;
+
+if ($sleep && $seconds) {
+  sleep($seconds);
+}
+
 $usblight = new usblight($ini['server_url']);
 $usblight->setColor($color);
+
+if (!$sleep && $seconds) {
+  exec("nohup php " . __FILE__ . " 0 " . $seconds. " 1 >/dev/null 2>&1  & echo $! &");
+}
