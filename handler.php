655ef8030dae884ea52c1d355dfb48a533d90606
Kasper Hartwich <kha@concepy.com>
2013-02-12 09:43:29 +0100

    Use raspberrypi exe.

--- handler.php
+++ handler.php
@@ -5,7 +5,6 @@ include dirname(__FILE__) . '/usblight.php';
 if (!file_exists(dirname(__FILE__) . '/usblight.ini')) {die('usblight.ini missing');}
 $ini = parse_ini_file(dirname(__FILE__) . '/usblight.ini');
 
-$os_supported = array('osx');
 $usblight = new usblight($ini['server_url']);
 $current_color = 0;
 
@@ -13,8 +12,11 @@ while (true) {
   $color = $usblight->getColor();
   if ($current_color != $color) {
     $current_color = $color;
-    if (in_array($ini['os'], $os_supported)) {
-      exec(dirname(__FILE__) . '/light-' . $ini['os'] . ' ' . $color);
+    if ($ini['os']=='osx') {
+      exec(dirname(__FILE__) . '/light-osx ' . $color);
+    } else if ($ini['os']=='raspberrypi') {
+      $colors = array(0 => 'off', 1 => 'blue', 2 => 'red', 3 => 'green', 4 => 'lightblue', 5 => 'purple', 6 => 'lightgreen', 7 => 'white');
+      exec(dirname(__FILE__) . '/light-raspberrypi ' . $colors[$color]);
     }
     echo 'Changed color: ' . $color . "\n";
   }
